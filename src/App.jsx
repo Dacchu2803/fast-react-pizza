2618c28210b2f113da904dc946cc6a256d4a4717
Dacchu2803 <darshanj2803@gmail.com>
2025-06-07 13:17:28 +0530

    Code changes for common layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import {RouterProvider,createBrowserRouter} from "react-router-dom"; 
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+
+
+const router = createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children:[
+       {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"/menu",
+    element:<Menu/>
+  },
+  {
+    path:"/order/new",
+    element:<CreateOrder />
+  },
+  {
+    path:"/cart",
+    element:<Cart/>
+  },
+  {
+    path:"/order/:orderId",
+    element:<Order />
+  }
+    ]
+  },
+ 
+]);
+
 function App(){
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router}/>
 }
 
 export default App;
